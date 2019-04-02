7a3a1cd66e0b9b46fabe294651ea37ea3806f530
Jorge Viana <secoviana@gmail.com>
2019-04-02 12:45:22 +0200

    first version of golden master

---
+++ java/src/test/java/com/adaptionsoft/games/trivia/GoldenMaster.java
@@ -0,0 +1,38 @@
+package com.adaptionsoft.games.trivia;
+
+import com.adaptionsoft.games.trivia.runner.GameRunner;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+
+public class GoldenMaster {
+
+    private PrintStream out;
+    private PrintStream defaultOut;
+
+    @Before
+    public void setUp() throws Exception {
+        defaultOut = System.out;
+        out = new PrintStream(new FileOutputStream("golden_master.txt"));
+        System.setOut(out);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        out.flush();
+        out.close();
+
+        System.setOut(defaultOut);
+    }
+
+    @Test
+    public void run_game_1000_times() throws FileNotFoundException {
+
+        GameRunner.main(null);
+
+    }
+}
